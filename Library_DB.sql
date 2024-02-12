6306a65bb4c69ac81faf737a937db7f47bf07ea7
AmanBadholia <159482577+AmanBadholia@users.noreply.github.com>
2024-02-12 20:46:20 +0530

    Update Library_DB.sql

--- Library_DB.sql
+++ Library_DB.sql
@@ -1,18 +1,19 @@
 create database LibraryDB;
 use LibraryDB;
+Show databases;
 
 create table Books 
-( Book_ID smallint primary key ,
-Title varchar(64) not null ,
+( Book_ID int auto_increment primary key ,
+Title varchar(200) not null ,
 Author varchar(64) not null,
-Genre varchar(64) not null,
-ISBN int not null,
-Published_Year year not null,
-Quantity_Available int not null);
+Genre varchar(200) not null,
+ISBN int unsigned not null,
+Published_Year smallint not null,
+Quantity_Available int unsigned not null);
 Desc Books;
 
 create table Users 
-( User_ID smallint primary key ,
+( User_ID smallint auto_increment primary key ,
 FirstName varchar(64) not null ,
 LastName varchar(64) not null,
 Email varchar(64) not null,
@@ -21,12 +22,13 @@ PhoneNumber bigint not null);
 Desc Users;
 
 create table Transactions
-( Transaction_ID smallint primary key ,
-User_ID smallint,
-Foreign key(User_ID) references Users(User_ID),
-Book_ID smallint,
-Foreign key(Book_ID) references Books(Book_ID),
+( Transaction_ID smallint auto_increment primary key ,
+UID smallint not null,
+Foreign key(UID) references Users(User_ID),
+BID int not null,
+Foreign key(BID) references Books(Book_ID),
 Borrow_Date datetime not null,
+Due_Date datetime not null,
 Return_Date datetime not null);
 
 Desc Transactions;
